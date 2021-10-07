5aca05d6dadeb2978f20cfe66edf2b8cb43893e0
flaviopangracio <flavio.pangracio@tembici.com>
2021-10-07 16:44:03 -0300

    adjust glue in path names

--- src/main.R
+++ src/main.R
@@ -13,14 +13,14 @@ encoding <- "UTF-8"
 ########################################
 
 # Carregar pacotes
-source("../config/libs/install_and_load_packages.R"), encoding = encoding)
+source(glue("{project_root_path}/config/libs/install_and_load_packages.R"), encoding = encoding)
 
 ########################################
 ######      AUTHENTICATIONS       ######
 ########################################
 
 # Fazer conexão com o banco de dados
-source("../config/authentication/authentication.R"),  encoding = encoding)
+source(glue("{project_root_path}/config/authentication/authentication.R"), encoding = encoding)
 
 ########################################
 ######     LEITURA DE DADOS       ######
@@ -29,12 +29,12 @@ source("../config/authentication/authentication.R"),  encoding = encoding)
 # update da análise ou usar dados do GCS
 update_data <- TRUE
 
-# Leitura dos dados 
+# Leitura dos dados
 
 if(update_data){
-  
-  source("data/loaddata.R"), encoding = encoding)
-  
+
+  source(glue("{project_root_path}/src/data/loaddata.R"), encoding = encoding)
+
   message("Você está conectado ao BQ e pronto para usar as tabelas ...")
 
 } else {
@@ -45,11 +45,11 @@ if(update_data){
 #####   ANÁLISE EXPLORATÓRIA      ######
 ########################################
 
-##### 
+#####
 ##  JOIN DAS TABELAS E GERAÇÃO DOS DADOS E GRÁFICOS PARA AS ANÁLISES
 #####
 
-source("analysis/analysis.R"), encoding = encoding)
+source(glue("{project_root_path}/src/analysis/analysis.R"), encoding = encoding)
 
 ########################################
 #####      RELATÓRIOS HTML        ######
@@ -58,10 +58,11 @@ source("analysis/analysis.R"), encoding = encoding)
 ####
 ## Relatório
 ####
+
 rmarkdown::render(
-  input = "reports/report.Rmd",
+  input = glue("{project_root_path}/src/reports/report.Rmd"),
   encoding = encoding, clean = T,
-  output_file = "../exports/reports/report.html"
+  output_file = glue("{project_root_path}/exports/reports/report.html")
 )
 
 ####
@@ -69,7 +70,7 @@ rmarkdown::render(
 ####
 
 rmarkdown::render(
-  input = "reports/slides.Rmd",
+  input = glue("{project_root_path}/src/reports/slides.Rmd"),
   encoding = encoding, clean = T,
-  output_file = "../exports/slides/slides.html"
+  output_file = glue("{project_root_path}/exports/slides/slides.html")
 )
